9844937ef64035951c9a213c51e215ef180ca053
Jeffrey Shen <jeffdshen@gmail.com>
2021-10-26 23:05:28 -0700

    rm superglue combined.zip after downloading

--- experiments/download_data.sh
+++ experiments/download_data.sh
@@ -4,3 +4,5 @@
 mkdir -p save/data
 wget -q https://dl.fbaipublicfiles.com/glue/superglue/data/v2/combined.zip -O save/data/combined.zip
 unzip -q save/data/combined.zip -d save/data/
+rm save/data/combined.zip
+
