5861ab7ca0e0c180b2a97eca80045ec0ddc594c7
Jeffrey Shen <jeffdshen@gmail.com>
2021-11-05 15:45:56 -0700

    Add initial models

---
+++ models/roberta.py
@@ -0,0 +1,119 @@
+# Copyright (c) Jeffrey Shen
+
+"""RoBERTa Model with LM and classification head"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.cuda.amp as amp
+
+import models.transformer as T
+
+
+class RoBERTa(nn.Module):
+    def __init__(
+        self,
+        dim,
+        n_heads,
+        ff_dim,
+        activation,
+        dropout,
+        attn_dropout,
+        act_dropout,
+        n_layers,
+        max_positions,
+        max_tokens,
+        padding_idx,
+        ignore_idx,
+        prenorm=False,
+    ):
+        super().__init__()
+        embed_tokens = T.LearnedTokenEmbedding(max_tokens, dim, padding_idx)
+        self.embed = T.TransformerEncoderEmbedding(
+            embed_tokens,
+            T.LearnedPositionalEmbedding(max_positions, dim),
+            dim=dim,
+            dropout=dropout,
+            layer_norm=(not prenorm),
+        )
+
+        if not prenorm:
+            encoder_layer = T.TransformerEncoderLayer(
+                dim=dim,
+                n_heads=n_heads,
+                ff_dim=ff_dim,
+                activation=activation,
+                dropout=dropout,
+                attn_dropout=attn_dropout,
+                act_dropout=act_dropout,
+            )
+        else:
+            encoder_layer = T.TransformerPrenormEncoderLayer(
+                dim=dim,
+                n_heads=n_heads,
+                ff_dim=ff_dim,
+                activation=activation,
+                dropout=dropout,
+                attn_dropout=attn_dropout,
+                act_dropout=act_dropout,
+            )
+
+        self.encoder = T.TransformerEncoder(
+            encoder_layer,
+            n_layers=n_layers,
+        )
+
+        if prenorm:
+            self.final_layer_norm = nn.LayerNorm(dim)
+        else:
+            self.final_layer_norm = None
+
+        self.head = T.LMHead(
+            dim=dim,
+            output_tokens=max_tokens,
+            activation=activation,
+            weight=embed_tokens.embed.weight,
+        )
+        self.ignore_idx = ignore_idx
+        self.apply(lambda mod: T.init_params_bert(mod, 0.02))
+
+    # (N, S), (N, S), (N, S) -> (N, S, O)
+    @amp.autocast()
+    def forward(self, x, positions=None, padding_mask=None):
+        x = x.transpose(0, 1)
+        if positions is None:
+            positions = T.get_positions(x)
+        else:
+            positions = positions.transpose(0, 1)
+        x = self.embed(x, positions)
+        x = self.encoder.forward(x, key_padding_mask=padding_mask)
+        if self.final_layer_norm is not None:
+            x = self.final_layer_norm(x)
+        x = self.head(x)
+        x = x.transpose(0, 1)
+        return x
+
+    # (N, S, O), (N, S) -> (N, S, O)
+    def mask_scores(self, x, padding_mask):
+        return self.head.mask_scores(x, padding_mask)
+
+    # (N, S, O) -> (N, S)
+    def get_top(self, x):
+        return self.head.get_top(x)
+
+    # (M, O) -> (M, K). M < N x S
+    # Since this gets masked prior, N, S gets folded into 1 dim.
+    def sample(self, x, k, alpha=1.0):
+        return self.head.sample(x, k, alpha=alpha)
+
+    # (N, S, O) -> (N, S, O*)
+    def get_log_prob(self, x):
+        return self.head.get_log_prob(x)
+
+    # (N, S, O) -> (N, S, O*)
+    def get_prob(self, x):
+        return self.head.get_prob(x)
+
+    # (N, S, O), (N, O) -> (1, )
+    def get_loss(self, scores, y):
+        return self.head.get_loss(scores, y, self.ignore_idx)
---
+++ models/transformer.py
@@ -0,0 +1,261 @@
+# Copyright (c) Jeffrey Shen
+
+"""Various building blocks for transformer architectures"""
+
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LearnedTokenEmbedding(nn.Module):
+    """Learned token embedding
+
+    Args:
+        num_words (int): Vocab size
+        embed_dim (int): Output size of the embedding
+        padding_idx (int): Padding index
+    """
+
+    def __init__(self, num_words, embed_dim, padding_idx):
+        super().__init__()
+        self.embed = nn.Embedding(num_words, embed_dim, padding_idx)
+
+    def forward(self, x):
+        emb = self.embed(x)
+        return emb
+
+
+class LearnedPositionalEmbedding(nn.Module):
+    """Learned positional embedding
+
+    Args:
+        max_positions (int): Max number of positions
+        embed_dim (int): Output size of the embedding
+    """
+
+    def __init__(self, max_positions, embed_dim):
+        super().__init__()
+        self.embed = nn.Embedding(max_positions, embed_dim)
+
+    def forward(self, x):
+        emb = self.embed(x)
+        return emb
+
+
+def get_activation_fn(activation):
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+    else:
+        raise RuntimeError("Unsupported activation function: {}".format(activation))
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(
+        self, dim, n_heads, ff_dim, activation, dropout, attn_dropout, act_dropout
+    ):
+        super().__init__()
+        self.self_attn = nn.MultiheadAttention(dim, n_heads, dropout=attn_dropout)
+        self.self_attn_dropout = nn.Dropout(dropout)
+        self.self_attn_norm = nn.LayerNorm(dim)
+
+        self.linear1 = nn.Linear(dim, ff_dim)
+        self.activation = get_activation_fn(activation)
+        self.act_dropout = nn.Dropout(act_dropout)
+        self.linear2 = nn.Linear(ff_dim, dim)
+        self.ff_dropout = nn.Dropout(dropout)
+
+        self.ff_norm = nn.LayerNorm(dim)
+
+    # ((S, N, E), (N, S), (S, S)) -> (S, N, E)
+    def forward(self, x, key_padding_mask=None, attn_mask=None):
+        residual = x
+        x, _ = self.self_attn(
+            x, x, x, attn_mask=attn_mask, key_padding_mask=key_padding_mask
+        )
+        x = self.self_attn_dropout(x)
+        x = residual + x
+        x = self.self_attn_norm(x)
+
+        residual = x
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.act_dropout(x)
+        x = self.linear2(x)
+        x = self.ff_dropout(x)
+        x = residual + x
+        x = self.ff_norm(x)
+
+        return x
+
+
+class TransformerPrenormEncoderLayer(nn.Module):
+    def __init__(
+        self, dim, n_heads, ff_dim, activation, dropout, attn_dropout, act_dropout
+    ):
+        super().__init__()
+        self.self_attn = nn.MultiheadAttention(dim, n_heads, dropout=attn_dropout)
+        self.self_attn_dropout = nn.Dropout(dropout)
+        self.self_attn_norm = nn.LayerNorm(dim)
+
+        self.linear1 = nn.Linear(dim, ff_dim)
+        self.activation = get_activation_fn(activation)
+        self.act_dropout = nn.Dropout(act_dropout)
+        self.linear2 = nn.Linear(ff_dim, dim)
+        self.ff_dropout = nn.Dropout(dropout)
+
+        self.ff_norm = nn.LayerNorm(dim)
+
+    # ((S, N, E), (N, S), (S, S)) -> (S, N, E)
+    def forward(self, x, key_padding_mask=None, attn_mask=None):
+        residual = x
+        x = self.self_attn_norm(x)
+        x, _ = self.self_attn(
+            x, x, x, attn_mask=attn_mask, key_padding_mask=key_padding_mask
+        )
+        x = self.self_attn_dropout(x)
+        x = residual + x
+
+        residual = x
+        x = self.ff_norm(x)
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.act_dropout(x)
+        x = self.linear2(x)
+        x = self.ff_dropout(x)
+        x = residual + x
+
+        return x
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, layer, n_layers):
+        super().__init__()
+        self.layers = nn.ModuleList([copy.deepcopy(layer) for i in range(n_layers)])
+
+    # ((S, N, E), (N, S), (S, S)) -> (S, N, E)
+    def forward(self, x, key_padding_mask=None, attn_mask=None):
+        output = x
+        for layer in self.layers:
+            output = layer(
+                output, key_padding_mask=key_padding_mask, attn_mask=attn_mask
+            )
+        return output
+
+
+class TransformerEncoderEmbedding(nn.Module):
+    def __init__(self, embed_tokens, embed_position, dim, dropout, layer_norm=True):
+        super().__init__()
+        self.embed_tokens = embed_tokens
+        self.embed_position = embed_position
+        if layer_norm:
+            self.layer_norm = nn.LayerNorm(dim)
+        else:
+            self.layer_norm = None
+        self.dropout = nn.Dropout(dropout)
+
+    # ((S, N), (S, N)) -> (S, N, E)
+    def forward(self, x, positions):
+        x = self.embed_tokens(x)
+        x = x + self.embed_position(positions)
+        if self.layer_norm is not None:
+            x = self.layer_norm(x)
+        x = self.dropout(x)
+        return x
+
+
+class LMHead(nn.Module):
+    def __init__(self, dim, output_tokens, activation, weight=None):
+        super().__init__()
+        self.ff_linear = nn.Linear(dim, dim)
+        self.activation = get_activation_fn(activation)
+        self.layer_norm = nn.LayerNorm(dim)
+
+        self.output = nn.Linear(dim, output_tokens)
+        if weight is not None:
+            self.output.weight = weight
+
+    # (S, N, E) -> (S, N, O)
+    def forward(self, x):
+        x = self.ff_linear(x)
+        x = self.activation(x)
+        x = self.layer_norm(x)
+
+        x = self.output(x)
+        return x
+
+    # (N, S, O), (N, S) -> (N, S, O)
+    @staticmethod
+    def mask_scores(x, padding_mask):
+        # Swap the dimension back to original order for speed reasons
+        swap = not x.is_contiguous()
+        if swap:
+            x = x.transpose(0, 1)
+            padding_mask = padding_mask.transpose(0, 1)
+        masked = x.masked_fill(padding_mask.unsqueeze(-1), float("-inf"))
+        if swap:
+            return masked.transpose(0, 1)
+        return masked
+
+    # (N, S, O) -> (N, S)
+    @staticmethod
+    def get_top(x):
+        return torch.argmax(x, dim=-1)
+
+    # (N, O) -> (N, K)
+    @staticmethod
+    def sample(x, k, alpha=1.0):
+        s = F.softmax(x / alpha, dim=-1)
+        m = torch.multinomial(s.view(-1, s.size(-1)), k, replacement=True)
+        return m.view(s.size(0), k)
+
+    # (N, S, O) -> (N, S, O*)
+    @staticmethod
+    def get_log_prob(x):
+        return F.log_softmax(x, dim=-1)
+
+    # (N, S, O) -> (N, S, O*)
+    @staticmethod
+    def get_prob(x):
+        return F.softmax(x, dim=-1)
+
+    # ((N, S, O), (N, S)) -> (1, )
+    @staticmethod
+    def get_loss(scores, y, ignore_idx):
+        # Swap the dimension back to original order for speed reasons
+        if not scores.is_contiguous():
+            scores = scores.transpose(0, 1)
+            y = y.transpose(0, 1)
+        return F.cross_entropy(scores.transpose(1, -1), y, ignore_index=ignore_idx)
+
+
+# (S, N) -> (S, N=1)
+def get_positions(x):
+    positions = torch.arange(x.shape[0], dtype=torch.long, device=x.device)
+    return positions.unsqueeze(1)
+
+
+# (N, S) -> (N, S)
+def get_padding_mask(x, padding_idx):
+    return x.eq(padding_idx)
+
+
+def init_params_bert(module, std):
+    """
+    BERT initialization using normal distribution.
+    """
+    if isinstance(module, (nn.Linear, nn.Embedding)):
+        if module.weight.requires_grad:
+            nn.init.normal_(module.weight, std=std)
+
+    if isinstance(module, nn.Embedding):
+        if module.padding_idx is not None:
+            with torch.no_grad():
+                module.weight[module.padding_idx].fill_(0)
+
+    if isinstance(module, nn.Linear):
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
