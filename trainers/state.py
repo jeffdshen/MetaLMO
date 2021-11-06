1cbd08b383a468ce3ec9d949cc4ea7fece254ef4
Jeffrey Shen <jeffdshen@gmail.com>
2021-11-06 16:36:30 -0700

    Add trainer state tracking

---
+++ trainers/state.py
@@ -0,0 +1,139 @@
+# Copyright (c) Jeffrey Shen
+
+import os
+import random
+import numpy as np
+import torch
+
+class TrainerState:
+    def __init__(self, log=None):
+        super().__init__()
+        self.state = {}
+        self.objs = {}
+        self.log = log
+
+    def load_state_dict(self, state_dict):
+        self.state = state_dict
+
+    def is_reloading(self):
+        return bool(self.state)
+
+    def track_object(self, name, obj):
+        self.objs[name] = obj
+        if name in self.state:
+            if self.log is not None:
+                self.log.info("Reloading {}".format(name))
+            obj.load_state_dict(self.state.pop(name))
+            return True
+        return False
+
+    def state_dict(self):
+        state = {}
+        for name, obj in self.objs.items():
+            state[name] = obj.state_dict()
+        return state
+
+
+class KeepSimpleState:
+    def __init__(self, keep, **kwargs):
+        self.__dict__.update(kwargs)
+        self.__keep = keep
+
+    @staticmethod
+    def init_from(other, keep):
+        return KeepSimpleState(keep, **other.__dict__)
+
+    def state_dict(self):
+        return self.__dict__
+
+    def load_state_dict(self, state):
+        keep = self.__keep
+        keep_dict = {k: self.__dict__[k] for k in self.__keep}
+        self.__dict__ = state
+        self.__dict__.update(keep_dict)
+        self.__keep = keep
+
+
+class SimpleState:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    @staticmethod
+    def init_from(other):
+        return SimpleState(**other.__dict__)
+
+    def state_dict(self):
+        return self.__dict__
+
+    def load_state_dict(self, state):
+        self.__dict__ = state
+
+
+class RandomState:
+    def __init__(self, seed):
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    def state_dict(self):
+        return {
+            "random": random.getstate(),
+            "np.random": np.random.get_state(),
+            "torch.random": torch.random.get_rng_state(),
+            "torch.cuda.random": torch.cuda.get_rng_state_all(),
+        }
+    
+    def load_state_dict(self, state):
+        random.setstate(state["random"])
+        np.random.set_state(state["np.random"])
+        torch.set_rng_state(state["torch.random"])
+        torch.cuda.set_rng_state_all(state["torch.cuda.random"])
+
+
+class ModelSaver:
+    def __init__(self, save_dir, metric_names, maximize_metric, log):
+        super().__init__()
+
+        self.save_dir = save_dir
+        self.metric_names = metric_names
+        self.maximize_metric = maximize_metric
+        self.best_vals = {}
+        self.log = log
+        maximize = ", ".join(m for m in metric_names if maximize_metric[m])
+        minimize = ", ".join(m for m in metric_names if not maximize_metric[m])
+        if maximize:
+            self.log.info(f"Best model saver will maximize {maximize}")
+        if minimize:
+            self.log.info(f"Best model saver will minimize {minimize}")
+
+    def save(self, step, model, results):
+        ckpt_dict = {
+            "model": model.state_dict(),
+            "step": step,
+        }
+
+        for metric in self.metric_names:
+            if metric not in results:
+                continue
+            value = results[metric]
+            maximize = self.maximize_metric[metric]
+            if metric in self.best_vals:
+                best_val = self.best_vals[metric]
+                if maximize and best_val >= value:
+                    continue
+                if not maximize and best_val <= value:
+                    continue
+
+            # value is better than best_val
+            self.best_vals[metric] = value
+            best_path = os.path.join(self.save_dir, f"best_{metric}.pth.tar")
+            self.log.info(f"New best checkpoint for metric {metric} at step {step}...")
+            torch.save(ckpt_dict, best_path)
+
+    @staticmethod
+    def load_model(model, checkpoint_path, device, strict):
+        ckpt_dict = torch.load(checkpoint_path, map_location=device)
+        model.load_state_dict(ckpt_dict["model"], strict=strict)
+        step = ckpt_dict["step"]
+        return model, step
---
+++ trainers/test_state.py
@@ -0,0 +1,126 @@
+import unittest
+
+import random
+import torch
+import numpy as np
+
+from .state import TrainerState, SimpleState, KeepSimpleState, RandomState
+
+
+class SimpleStateTestCase(unittest.TestCase):
+    def test_init(self):
+        x = SimpleState(a=10, b=100)
+        self.assertEqual(x.a, 10)
+        self.assertEqual(x.b, 100)
+
+    def test_state_dict(self):
+        x = SimpleState(a=10, b=100)
+        self.assertEqual(x.state_dict(), {"a": 10, "b": 100})
+
+    def test_modify(self):
+        x = SimpleState(a=10, b=100)
+        x.a = 11
+        x.c = 99
+        self.assertEqual(x.state_dict(), {"a": 11, "b": 100, "c": 99})
+
+    def test_load_state_dict(self):
+        x = SimpleState(a=10, b=100)
+        state = {"a": 11, "c": 99}
+        x.load_state_dict(state)
+        self.assertEqual(x.state_dict(), {"a": 11, "c": 99})
+
+    def test_init_from(self):
+        x = SimpleState(a=10, b=100)
+        y = SimpleState.init_from(x)
+        self.assertEqual(y.state_dict(), {"a": 10, "b": 100})
+
+
+class KeepSimpleStateTestCase(unittest.TestCase):
+    def test_init(self):
+        x = KeepSimpleState(["a"], a=10, b=100)
+        self.assertEqual(x.a, 10)
+        self.assertEqual(x.b, 100)
+
+    def test_state_dict(self):
+        x = KeepSimpleState(["a"], a=10, b=100)
+        y = x.state_dict().copy()
+        y.update({"a": 10, "b": 100})
+        self.assertEqual(x.state_dict(), y)
+
+    def test_modify(self):
+        x = KeepSimpleState(["a"], a=10, b=100)
+        x.a = 11
+        x.c = 99
+        y = x.state_dict().copy()
+        y.update({"a": 11, "b": 100, "c": 99})
+        self.assertEqual(x.state_dict(), y)
+
+    def test_load_state_dict(self):
+        x = KeepSimpleState(["a", "b"], a=10, b=100, c=1000)
+        state = {"a": 11, "c": 99}
+        x.load_state_dict(state)
+        self.assertEqual(x.a, 10)
+        self.assertEqual(x.b, 100)
+        self.assertEqual(x.c, 99)
+
+    def test_init_from(self):
+        x = KeepSimpleState(["a"], a=10, b=100)
+        y = KeepSimpleState.init_from(x, ["a", "b"])
+        y.load_state_dict({"a": 9, "b": 99})
+        self.assertEqual(y.a, 10)
+        self.assertEqual(y.b, 100)
+
+
+class RandomStateTestCase(unittest.TestCase):
+    def test_init(self):
+        _ = RandomState(seed=42)
+        a = random.randint(0, 100)
+        b = torch.randint(0, 100, size=(1,)).item()
+        c = np.random.randint(0, 100)
+        _ = RandomState(seed=42)
+        self.assertEqual(random.randint(0, 100), a)
+        self.assertEqual(torch.randint(0, 100, size=(1,)).item(), b)
+        self.assertEqual(np.random.randint(0, 100), c)
+        
+    def test_state_dict(self):
+        x = RandomState(seed=42)
+        a_old = random.randint(0, 100)
+        b_old = torch.randint(0, 100, size=(1,)).item()
+        c_old = np.random.randint(0, 100)
+        state = x.state_dict()
+        a = random.randint(0, 100)
+        b = torch.randint(0, 100, size=(1,)).item()
+        c = np.random.randint(0, 100)
+        self.assertNotEqual(a, a_old)
+        self.assertNotEqual(b, b_old)
+        self.assertNotEqual(c, c_old)
+        x.load_state_dict(state)
+        self.assertEqual(random.randint(0, 100), a)
+        self.assertEqual(torch.randint(0, 100, size=(1,)).item(), b)
+        self.assertEqual(np.random.randint(0, 100), c)
+
+
+
+class TrainerStateTestCase(unittest.TestCase):
+    def test_reload(self):
+        x = TrainerState()
+        self.assertFalse(x.is_reloading())
+        obj1 = SimpleState(a = "hello", b = "world")
+        x.track_object("obj1", obj1)
+        obj2 = SimpleState(a = "foo", c = "bar")
+        x.track_object("obj2", obj2)
+
+        obj1.c = "!"
+        obj2.d= "baz"
+        state = x.state_dict()
+
+        x = TrainerState()
+        x.load_state_dict(state)
+        self.assertTrue(x.is_reloading())
+        obj1 = SimpleState()
+        x.track_object("obj1", obj1)
+        obj2 = SimpleState()
+        x.track_object("obj2", obj2)
+        self.assertEqual(obj1.state_dict(), {"a": "hello", "b": "world", "c": "!"})
+        self.assertEqual(obj2.state_dict(), {"a": "foo", "c": "bar", "d": "baz"})
+        self.assertFalse(x.is_reloading())
