5803c590e9c4117d01c6263af07eaecff8a585fc
Jeffrey Shen <jeffdshen@gmail.com>
2021-11-07 02:18:44 -0800

    Use curl instead of wget for download_data

--- experiments/download_data.sh
+++ experiments/download_data.sh
@@ -2,7 +2,7 @@
 
 # SuperGLUE
 mkdir -p save/data
-wget -q https://dl.fbaipublicfiles.com/glue/superglue/data/v2/combined.zip -O save/data/combined.zip
+curl --silent https://dl.fbaipublicfiles.com/glue/superglue/data/v2/combined.zip -o save/data/combined.zip
 unzip -q save/data/combined.zip -d save/data/
 rm save/data/combined.zip
 
